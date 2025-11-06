e5d676a74262ccb798ef2203f9767d448c9eb33a
Bart Riemens <briemens@crafity.com>
2025-11-06 22:27:20 +0100

    Implement new logo for constellation

--- src/components/layout/header/logo.tsx
+++ src/components/layout/header/logo.tsx
@@ -3,7 +3,18 @@ import styles from './logo.module.css'
 export function Logo() {
   return (
     <div className={styles['logo-container']}>
-      <span className={styles['terminal-prompt']}>$</span>
+      <picture className={styles['logo-icon']}>
+        <source
+          media="(prefers-color-scheme: light)"
+          srcSet="/logo-constellation-icon-64-light.svg"
+        />
+        <img
+          alt="Crafity"
+          height="64"
+          src="/logo-constellation-icon-64.svg"
+          width="64"
+        />
+      </picture>
       <div className={styles['logo-wrapper']}>
         <div className={styles.logo}>CRAFITY</div>
         <div className={styles.tagline}>// Built with passion</div>
